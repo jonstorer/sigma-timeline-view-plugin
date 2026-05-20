062d14e0da8c072661b3a526dc3a1b3ef9a1d00f
Jonathon Storer <jonathon@sigmacomputing.com>
2026-05-20 12:57:57 -0700

    wip

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { client, SigmaClientProvider } from '@sigmacomputing/plugin'
+import 'vis-timeline/styles/vis-timeline-graph2d.css'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <SigmaClientProvider client={client}>
+      <App />
+    </SigmaClientProvider>
+  </StrictMode>,
+)
